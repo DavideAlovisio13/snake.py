4b3f2690d8a159a9da180ced1741c67dcd483e9f
DavideAlovisio13 <alovisiod@gmail.com>
2024-11-26 16:07:51 +0100

    second commit

--- snake.py
+++ snake.py
@@ -8,7 +8,14 @@ from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN  # For detecting arrow 
 curses.initscr()
 
 # Create a new window of 30 rows and 60 columns, starting at (0, 0) position
-window = curses.newwin(30, 60, 0, 0)
+window = curses.newwin(30, 60, 0, 0, curses.init_pair)
+
+#colors
+curses.start_color()
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)  # snake 
+curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)    # food
+curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # text/border
+
 
 # Enable keypad mode for the window (to handle special keys like arrow keys)
 window.keypad(True)
@@ -16,13 +23,13 @@ window.keypad(True)
 # Draw a border around the window
 window.border(0)
 
-window.timeout(10)  # Set the timeout for the window to 10 milliseconds
 
 snake = [[15, 13], [15, 12], [15, 11]]  # Starting position of the snake
 food = [5, 35]  # Starting position of the food
 lookFor = KEY_DOWN  # Initial direction of the snake
 points = 0  # Initial score
-window.addch(food[0], food[1], "O")  # Draw the food on the window
+window.timeout(max(10, 100 - points * 5))  # Adjust the speed of the game based on the score
+window.addch(food[0], food[1], "O", curses.color_pair(2))  # Draw the food on the window
 
 while True:
     window.addstr(0, 14, "Points: " + str(points) + '')  # Display the current score
@@ -51,13 +58,13 @@ while True:
             food = [randint(1, 28), randint(1, 58)]  # Generate a new random position for the food
             if food in snake:
                 food = []  # If the food is at the same position as the snake, generate a new position
-        window.addch(food[0], food[1], "O")  # Draw the food on the window
+        window.addch(food[0], food[1], "O", curses.color_pair(2))  # Draw the food on the window
         
     else:
         lastPiece = snake.pop() # Remove the last piece of the snake
         window.addch(lastPiece[0], lastPiece[1], " ")  # Clear the last piece of the snake from the window
     
-    window.addch(snake[0][0], snake[0][1], "x")  # Draw the snake on the window
+    window.addch(snake[0][0], snake[0][1], "x", curses.color_pair(1))  # Draw the snake on the window
 
 curses.endwin() # End the curses mode
 print("Game Over! Your final score is: " + str(points))  # Display the final
